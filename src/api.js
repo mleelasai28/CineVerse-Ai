2b7f864463fda2b382411b76c2b0b0b721233221
mleelasai28 <mleelasai28@gmail.com>
2026-06-04 17:57:33 +0530

    Initial Commit

--- src/api.js
+++ src/api.js
@@ -8,16 +8,10 @@ const instance = axios.create({
 
 export const requests = {
   fetchTrending: `/trending/all/week?api_key=${API_KEY}`,
-
   fetchAction: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
-
   fetchComedy: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
-
   fetchTopRated: `/movie/top_rated?api_key=${API_KEY}`,
-
   fetchUpcoming: `/movie/upcoming?api_key=${API_KEY}`,
-
-  fetchPopular: `/movie/popular?api_key=${API_KEY}`,
 };
 
 export default instance;
